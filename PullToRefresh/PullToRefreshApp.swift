dc332bf71fc8ee29258ab86d20df592db0f9ce11
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-06-08 21:30:30 +0300

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Ramill Ibragimov on 08.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
