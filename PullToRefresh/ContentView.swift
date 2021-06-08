fd9078a78f66898873844f165470abd7823f2e17
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-06-08 21:36:35 +0300

    init

--- PullToRefresh/ContentView.swift
+++ PullToRefresh/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var refreshed = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text(refreshed ? "Refreshed" : "Not refreshed")
+                ForEach(0..<10) { i in
+                    Text("\(i) index")
+                }
+            }
+            .navigationTitle("Pull to refresh")
+            // iOS 15
+//            .refreshable {
+//                refreshed = true
+//            }
+        }
     }
 }
 
